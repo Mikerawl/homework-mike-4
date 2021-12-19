fb33bc9f48a0679559c9040e187d53ee0d595aaf
Michael Rawlings <rawlingsm99@gmail.com>
2021-12-19 00:13:17 -0600

    first update of files for homework 4

---
+++ assets/script.js
@@ -0,0 +1,68 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+// When I press the start button I begin the test and start the times
+var message ='Game Over';
+// var words = message.split(' ');
+
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 5;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      alert("Game Over")
+    }
+  }, 1000);
+}
+
+// // Displays the message one word at a time
+// function displayMessage() {
+//   var wordCount = 0;
+
+//   // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//   var msgInterval = setInterval(function () {
+//     // If there are no more words left in the message
+//     if (words[wordCount] === undefined) {
+//       // Use `clearInterval()` to stop the timer
+//       clearInterval(msgInterval);
+//     } else {
+//       // Display one word of the message
+//       mainEl.textContent = words[wordCount];
+//       wordCount++;
+//     }
+    
+//   }, 1000);
+// }
+
+countdown();
+// GIVEN I am taking a code quiz
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+
+// WHEN the game is over
+// THEN I can save my initials and my score
